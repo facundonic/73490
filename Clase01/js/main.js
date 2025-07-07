bc0d73a3d790f90bf1acc0fd2d616f9ed6b3d526
Facundo <facugandini@hotmail.com>
2025-07-07 13:02:22 -0300

    Primer commit

---
+++ Clase01/js/main.js
@@ -0,0 +1,14 @@
+//Declaracion de variables
+let nombre;
+
+//Asignacion de variables
+nombre = "Rodrigo"
+console.log(nombre);
+
+//Inicializar variable
+let edad = 22;
+console.log(edad);
+
+//Constantes
+const nacimiento = 2002
+console.log(nacimiento);

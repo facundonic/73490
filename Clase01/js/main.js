39877a90acb2cd703872abae9a337b1d8f3ed13c
Facundo <facugandini@hotmail.com>
2025-07-07 13:43:26 -0300

    Readme

--- Clase01/js/main.js
+++ Clase01/js/main.js
@@ -4,6 +4,8 @@ let nombre;
 //Asignacion de variables
 nombre = "Rodrigo"
 console.log(nombre);
+nombre = "Facundo"
+console.log(nombre);
 
 //Inicializar variable
 let edad = 22;
@@ -12,3 +14,8 @@ console.log(edad);
 //Constantes
 const nacimiento = 2002
 console.log(nacimiento);
+
+alert("Hola, " + nombre + "! Tu edad es " + edad + " y naciste en " + nacimiento + ".");
+let numero = Number(prompt("Ingrese un número:"));
+let numero2 = Number(prompt("Ingrese otro número:"));
+console.log("La suma de los números es: " + (numero + numero2));

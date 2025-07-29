05441d04ff2126a992fe2789a1ff0b32fd643c64
Facundo <facugandini@hotmail.com>
2025-07-29 14:38:12 -0300

    pre entrega 2

--- js/main.js
+++ js/main.js
@@ -1,58 +1,54 @@
-// declarando variables (let y const).
-const ropa = ["Remera", "Pantalon", "Campera"];
-const valores = [600, 750, 1500];
-let total = 0;
+//? PRE-ENTREGA 3
 
-//function para mostrar la ropa disponibles.
-function mostrarRopa() {
-    alert(`Productos disponibles:
-    1- Remera
-    2- Pantalon
-    3- Campera`);
-}
+const shopContent = document.getElementById("shopContent");
+const seeCart = document.getElementById("seeCart");
+const modalContainer = document.getElementById("modalContainer");
+const quantityCart = document.getElementById("quantityCart");
 
-// function para realizar una switch case con las distintas elecciones posibles del usuario.
-function elegirRopa() {
-    let opciones = prompt("¿Qué prenda querés comprar?, Seleccione: 1, 2 o 3");
+let cart = JSON.parse(localStorage.getItem("dataCart")) || [];
 
-    switch (opciones) {
-        case "1":
-            total += valores[0];
-            alert("Seleccionaste REMERA por $600");
-            break;
-        case "2":
-            total += valores[1];
-            alert("Seleccionaste PANTALON por $750");
-            break;
-        case "3":
-            total += valores[2];
-            alert("Seleccionaste CAMPERA por $1500");
-            break;
-        default:
-            alert("Prenda no encontrada, vuelve a intentarlo.");
-            break;
+products.forEach((prod) => {
+    let content = document.createElement("div")
+    content.className = "gallery-products"
+    content.innerHTML = `
+    <img src="${prod.img}">
+    <h4>${prod.name}</h4>
+    <p class="price">S/${prod.price}</p>
+    `;
+
+    shopContent.append(content);
+
+    let shop = document.createElement("button");
+    shop.innerText = " Comprar ";
+    shop.className = "shop-btn bx-cart"
+
+    content.append(shop);
+
+    shop.addEventListener("click", () => {
+    const repeat = cart.some((repeatProduct) => repeatProduct.id === prod.id);
+
+    if (repeat){
+        cart.map ((product) => {
+            if (product.id === prod.id){
+                product.quantity++;
+            }
+        });
+    } else{
+        cart.push({
+            id: prod.id,
+            img: prod.img,
+            name: prod.name,
+            price: prod.price,
+            quantity: prod.quantity,
+        });
     }
+        console.log(cart);
+        cartCounter();
+        saveLocal();
+    });
+});
 
-    console.log("Total: $" + total);
-}
-
-// function para darle un inicio a la tienda, esto es lo primero que se va a mostrar.
-function iniciarTienda() {
-    alert("¡Bienvenido!, ¿desea comprar ropa?");
-
-    // variable para determinar el bucle de toda la "tienda de ropa".
-    let continuar = true;
-
-    while (continuar) {
-        mostrarRopa();
-        elegirRopa();
-
-        continuar = confirm("¿Querés comprar otro producto?");
-    }
-
-    //finalizando la compra.
-    alert("Gracias por tu compra. Total gastado: $" + total);
-    console.log("Compra finalizada. Total: $" + total);
-}
-
-iniciarTienda();
+//! Storage: almacenar datos
+const saveLocal = () => {
+    localStorage.setItem("dataCart", JSON.stringify(cart));
+};
